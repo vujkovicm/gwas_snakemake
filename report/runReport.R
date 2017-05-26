b38f3e900994624e811bb3872f23b4ac68e991db
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2017-05-26 15:22:46 -0400

    Update runReport.R

--- report/runReport.R
+++ report/runReport.R
@@ -1,13 +1,13 @@
-# args <- commandArgs(trailingOnly = TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
 setwd("/project/saleheenlab/snakemake/report/")
 
 # getting pandoc errors with executing html reports
 # conda install -c conda-forge pandoc=1.19.2
-# also have cairo installed
 
 library(rmarkdown)
 library(tools)
+library(knitr)
 
-rmarkdown::render(input = "report.Rmd", output_file = "summary/PROMIS_Summary.html")
+rmarkdown::render(input = "report.Rmd", output_file = paste("out/PROMIS_", args[1], "_Summary.html", sep = ""))
 
