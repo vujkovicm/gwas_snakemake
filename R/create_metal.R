3b588a48b9c756b4b7e35374e76fe4c733b76150
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2017-05-26 15:04:22 -0400

    Update create_metal.R

--- R/create_metal.R
+++ R/create_metal.R
@@ -1,37 +1,48 @@
 args <- commandArgs(trailingOnly = TRUE)
 
+#
+# METAL script creation
+#
+
+# set working directory
 dir = "/project/saleheenlab/snakemake/"
 
 # open
-sink(file = paste(dir, "script/promis_",  args[1], ".maf", args[2], ".metal", sep = ""), append = T)
+sink(file = paste(dir, "script/promis_",  args[1], ".metal", sep = ""), append = T)
 
-# cat
-cat("# PROMIS: GWAS COHORT 1 \n")
-cat("MARKER rsid \n")
-cat("ALLELE alleleA alleleB \n")
-cat("EFFECT frequentist_add_beta_1 \n")
-cat("STDERR frequentist_add_se_1 \n")
-cat("PVALUE frequentist_add_pvalue \n")
-cat("WEIGHT all_total \n")
-cat("GENOMICCONTROL ON \n")
-cat("SEPERATOR WHITESPACE \n")
-cat(paste("PROCESS ", dir, "out/PROMIS/imputed/summary/", args[1], "/", args[1], "_gwas1.snptest.maf", args[2], ".out \n", sep = ""))
-cat("\n")
+# additional variables to incorporate in metal output
+cat("CUSTOMVARIABLE all_total \n")
+cat("LABEL all_total as all_total \n")
 
-cat("# PROMIS: GWAS COHORT 2 \n")
-cat("MARKER rsid \n")
-cat("ALLELE alleleA alleleB \n")
-cat("EFFECT frequentist_add_beta_1 \n")
-cat("STDERR frequentist_add_se_1 \n")
-cat("PVALUE frequentist_add_pvalue \n")
-cat("WEIGHT all_total \n")
-cat("GENOMICCONTROL ON \n")
-cat("SEPERATOR WHITESPACE \n")
-cat(paste("PROCESS ", dir, "out/PROMIS/imputed/summary/", args[1], "/", args[1], "_gwas2.snptest.maf", args[2], ".out \n", sep = ""))
-cat("\n")
+cat("CUSTOMVARIABLE chromosome \n")
+cat("LABEL chromosome as chromosome \n")
 
-cat(paste("OUTFILE ", dir, "out/PROMIS/imputed/summary/", args[1], "/promis_", args[1], ".metal.maf", args[2], " .TBL \n", sep = ""))
-cat("ANALYZE \n")
+cat("CUSTOMVARIABLE position \n")
+cat("LABEL position as position \n")
+
+# options
 cat("SCHEME STDERR \n")
+cat("AVERAGEFREQ ON \n")
+cat("MINMAXFREQ ON \n")
+cat("COLUMNCOUNTING STRICT \n")
+cat("GENOMICCONTROL ON \n")
+
+# meta-analysis parameters
+cat("# PROMIS: GWAS COHORT 1 \n")
+cat("MARKER SNP \n")
+cat("ALLELE alleleA alleleB \n")
+cat("EFFECT frequentist_add_beta_1 \n")
+cat("STDERR frequentist_add_se_1 \n")
+cat("PVALUE frequentist_add_pvalue \n")
+cat("WEIGHT all_total \n")
+cat("FREQ all_maf \n")
+cat("SEPERATOR WHITESPACE \n")
+cat(paste("PROCESS ", dir, "out/PROMIS/imputed/summary/", args[1], "/", args[1], "_gwas1.snptest.filtered.out \n", sep = ""))
+cat(paste("PROCESS ", dir, "out/PROMIS/imputed/summary/", args[1], "/", args[1], "_gwas2.snptest.filtered.out \n", sep = ""))
+cat("\n")
+
+# export
+cat(paste("OUTFILE ", dir, "out/PROMIS/imputed/summary/", args[1], "/promis_", args[1], ".metal.", " .TBL \n", sep = ""))
+cat("ANALYZE HETEROGENEITY \n")
 cat("QUIT \n")
 sink()
