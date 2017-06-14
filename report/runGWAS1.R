2369770036e353206afc9bac0730c9ec5df1cc7f
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2017-06-14 16:45:06 -0400

    Create runGWAS1.R

---
+++ report/runGWAS1.R
@@ -0,0 +1,12 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+setwd("/project/saleheenlab/snakemake/report/")
+
+# getting pandoc errors with executing html reports
+# conda install -c conda-forge pandoc=1.19.2
+
+library(rmarkdown)
+library(tools)
+library(knitr)
+
+rmarkdown::render(input = "gwas1.Rmd", output_file = paste("out/GWAS1_", args[1], "_Summary.html", sep = ""))
